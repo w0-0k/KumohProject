5eb0760d783b21314017eadeed41352fd2d272e9
이종덕 <20141002@kumoh.ac.kr>
2019-06-09 02:16:00 +0900

    애플리케이션 서버 php문 및 사진 #2차 (야외활동지 추가 및 업데이트)

---
+++ OPENAIR/place/select_place.php
@@ -0,0 +1,36 @@
+﻿<?php
+	header('Content-Type: application/json;charset=UTF-8');
+	
+	require_once("../users/session.php");
+
+	require_once('../users/dbcon.php');
+	
+	error_reporting(0);
+		
+	$dbc = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if ($dbc == null) {
+		$response->error = "Connection Error";
+		exit(json_encode($response));
+	}
+	
+	mysqli_query($dbc, "set names utf8");
+	
+	$query = "SELECT category, name, image, sinfo, linfo, star, price, number, location, address FROM place_info ORDER BY name ASC";
+	$result = mysqli_query($dbc, $query);
+	if ($result == null) {
+		$response->error = "Querying Error";
+		exit(json_encode($response));
+	}
+	
+	$json = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+		$json['list'][] = $row;
+	}
+	
+	mysqli_free_result($result);
+	mysqli_close($dbc);
+	
+	exit(json_encode($json));
+	
+	
+?>
--- OPENAIR/review/insert_review.php
+++ OPENAIR/review/insert_review.php
@@ -18,7 +18,7 @@
 	$post = json_decode(file_get_contents('php://input'),true);
 	if ($post == null) {
 		// 로그인 페이지를 통해 로그인
-		if (empty($_POST['c_name']) || empty($_POST['user_email']) || empty($_POST['content']) || empty($_POST['rating'])) {
+		if (empty($_POST['c_name']) || empty($_POST['user_email']) || empty($_POST['content']) || empty($_POST['rating']) || empty($_POST['star'])) {
 			$response->error = "Input Error";
 			exit(json_encode($response));
 		}
@@ -27,12 +27,14 @@
 		$user_email = mysqli_real_escape_string($dbc, trim($_POST['user_email']));
 		$content = mysqli_real_escape_string($dbc, trim($_POST['content']));
 		$rating = mysqli_real_escape_string($dbc, trim($_POST['rating']));
+		$star = mysqli_real_escape_string($dbc, trim($_POST['star']));
 	}
 	else { 	// JSON Object
 		$c_name = $post['c_name'];
 		$user_email  = $post['user_email'];
 		$content  = $post['content'];
 		$rating = $post['rating'];
+		$star = $post['star'];
 	}
 	
 	// TODO: 체크 안해도 되는지 확인 필요 (보안 문제 발생 가능)
@@ -50,10 +52,13 @@
 			"(null, '$c_name', '$user_email', '$content', '$rating')";
 	//echo $query; 
 	$result = mysqli_query($dbc, $query);
+
 	if ($result == null) {
 		$response->error = "Insertion Error";
 		exit(json_encode($response));
 	}	
+
+	$result = mysqli_query($dbc, "UPDATE place_info SET star='$star' WHERE name = '$c_name'");
 
 	$response->status = "Success";
 	exit(json_encode($response));
